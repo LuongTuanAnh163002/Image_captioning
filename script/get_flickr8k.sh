1557e8c5f02875a287622e09eb145d34b5617bb8
Lương Tuấn Anh <114805429+LuongTuanAnh163002@users.noreply.github.com>
2023-12-20 23:23:27 +0700

    Update get_flickr8k.sh 6

--- script/get_flickr8k.sh
+++ script/get_flickr8k.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 # Flickr8k dataset http://cocodataset.org
 # Download command: bash ./scripts/get_flickr8k.sh
-gdown 1P-32Vfy3-s8gaAxbLqTbjLAWlKDGzbTy
+
+# Download/unzip dataset
+d='./' # unzip directory
+file_id='1P-32Vfy3-s8gaAxbLqTbjLAWlKDGzbTy' # ID file
+url="https://drive.google.com/uc?export=download&id=$file_id"
+filename='flickr8k.zip'
+
+gdown $file_id
+
+echo 'Unzipping' $filename '...'
+unzip -q $filename -d $d
+
+echo 'Removing' $filename '...'
+rm $filename
+
+echo 'Download complete!'
